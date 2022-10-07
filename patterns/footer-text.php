bf4a83630595675609374316e09be4b2e1363a81
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-10-07 15:25:43 +0200

    updated footer area

--- patterns/footer-text.php
+++ patterns/footer-text.php
@@ -9,3 +9,7 @@
 <!-- wp:paragraph -->
 <p>&copy; <?php echo wp_kses_post( date_i18n( 'Y' ) . ' ' . get_bloginfo( 'name' ) ); ?></p>
 <!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray"}}}},"className":"flip-link-hover"} -->
+<p class="flip-link-hover has-link-color"><a href="#"><?php _e( 'Privacy Policy', 'lincoln' ); ?></a> | <a href="#"><?php _e( 'Imprint', 'lincoln' ); ?></a></p>
+<!-- /wp:paragraph -->
--- patterns/footer-widgets.php
+++ patterns/footer-widgets.php
@@ -6,18 +6,16 @@
 */
 ?>
 
-<!-- wp:columns {"style":{"spacing":{"padding":{"top":"2vw","bottom":"2vw"}}}} -->
-<div class="wp-block-columns" style="padding-top:2vw;padding-bottom:2vw">
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|80","left":"var:preset|spacing|70"},"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"bottom":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns" style="margin-bottom:var(--wp--preset--spacing--60);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
 
-	<!-- wp:column -->
-	<div class="wp-block-column">
+	<!-- wp:column {"width":"50%"} -->
+	<div class="wp-block-column" style="flex-basis:50%">
 
 		<!-- wp:group {"className":"footer-widget"} -->
 		<div class="wp-block-group footer-widget">
 
-			<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
-			<h3 class="has-white-color has-text-color has-large-font-size"><?php _e( 'About Lincoln', 'lincoln' ); ?></h3>
-			<!-- /wp:heading -->
+			<!-- wp:site-title {"level":0} /-->
 
 			<!-- wp:paragraph -->
 			<p><?php _e( 'Lincoln is a next generation block theme built for the new WordPress Full-Site-Editing experience. It comes with a clean and modern design and is packed with features, including a huge range of block patterns, multiple header and footer block template parts and support for global style variants.', 'lincoln' ); ?></p>
@@ -32,18 +30,6 @@
 	<!-- wp:column -->
 	<div class="wp-block-column">
 
-		<!-- wp:group {"className":"footer-widget"} -->
-		<div class="wp-block-group footer-widget">
-
-		<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
-		<h3 class="has-white-color has-text-color has-large-font-size"><?php _e( 'Categories', 'lincoln' ); ?></h3>
-		<!-- /wp:heading -->
-
-		<!-- wp:categories {"className":"is-style-flip-link-hover"} /-->
-
-		</div>
-		<!-- /wp:group -->
-
 	</div>
 	<!-- /wp:column -->
 
@@ -53,11 +39,27 @@
 		<!-- wp:group {"className":"footer-widget"} -->
 		<div class="wp-block-group footer-widget">
 
-		<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
-		<h3 class="has-white-color has-text-color has-large-font-size"><?php _e( 'Archives', 'lincoln' ); ?></h3>
-		<!-- /wp:heading -->
+			<!-- wp:heading {"textAlign":"right","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}},"fontSize":"medium"} -->
+			<h4 class="has-text-align-right has-medium-font-size" style="margin-top:var(--wp--preset--spacing--50)"><?php _e( 'Get in touch', 'lincoln' ); ?></h4>
+			<!-- /wp:heading -->
 
-		<!-- wp:archives {"className":"is-style-flip-link-hover"} /-->
+			<!-- wp:paragraph {"align":"right","className":"flip-link-hover"} -->
+			<p class="has-text-align-right flip-link-hover">
+				<a href="tel://0123456789">0123-456789</a><br/>
+				<a href="mailto:example@domain.com">example@domain.com</a><br/>
+				1234 Lincoln Street, California, USA
+			</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:social-links {"className":"is-style-primary-hover","layout":{"type":"flex","justifyContent":"right"}} -->
+			<ul class="wp-block-social-links is-style-primary-hover">
+
+				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
+				<!-- wp:social-link {"url":"https://facebook.com/","service":"facebook"} /-->
+				<!-- wp:social-link {"url":"https://instagram.com","service":"instagram"} /-->
+
+			</ul>
+			<!-- /wp:social-links -->
 
 		</div>
 		<!-- /wp:group -->
