db8cd101fdcc72bd824269a5e2f76811b311169c
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-10-08 12:12:11 +0200

    updated font families

--- functions.php
+++ functions.php
@@ -53,40 +53,6 @@ add_action( 'enqueue_block_editor_assets', 'lincoln_block_scripts' );
 
 
 /**
-* Enqueue theme fonts.
-*/
-function lincoln_theme_fonts() {
-	$fonts_url = lincoln_get_fonts_url();
-
-	// Load Fonts if necessary.
-	if ( $fonts_url ) {
-		require_once get_theme_file_path( 'inc/wptt-webfont-loader.php' );
-		wp_enqueue_style( 'lincoln-theme-fonts', wptt_get_webfont_url( $fonts_url ), array(), '20201110' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'lincoln_theme_fonts', 1 );
-add_action( 'enqueue_block_editor_assets', 'lincoln_theme_fonts', 1 );
-
-
-/**
- * Retrieve webfont URL to load fonts locally.
- */
-function lincoln_get_fonts_url() {
-	$font_families = array(
-		'Roboto:400,400italic,500,500italic,700,700italic',
-	);
-
-	$query_args = array(
-		'family'  => urlencode( implode( '|', $font_families ) ),
-		'subset'  => urlencode( 'latin,latin-ext' ),
-		'display' => urlencode( 'swap' ),
-	);
-
-	return apply_filters( 'lincoln_get_fonts_url', add_query_arg( $query_args, 'https://fonts.googleapis.com/css' ) );
-}
-
-
-/**
  * Change excerpt length for default posts
  *
  * @param int $length Length of excerpt in number of words.
